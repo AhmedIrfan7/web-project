c2ba57705ccb321449945f6d37c28db77e55d40f
AhmedIrfan7 <ahmedirfan4560@gmail.com>
2026-05-09 00:23:14 +0500

    Step 40: fix Mongoose async pre-save hook - remove next() callback (not needed in Mongoose 7+)

--- models/User.js
+++ models/User.js
@@ -37,11 +37,10 @@ const UserSchema = new Schema(
   { timestamps: true }
 );
 
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 UserSchema.methods.comparePassword = async function (candidate) {
